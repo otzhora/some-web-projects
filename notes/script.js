ca2f3ebabd69a8bbc38545eae9135077a90021ac
Yuriy Rogachev <y_rogachev@inbox.ru>
2017-11-17 15:17:48 +0300

    finised features

--- notes/script.js
+++ notes/script.js
@@ -40,8 +40,101 @@ function closeWindow(e, window, card, save)
 
         card.querySelector(".title").textContent = inputs[0].value
         card.querySelector(".text").textContent = inputs[1].value
+        saveImg(card, inputs[2].value)
+        changePriorities(card, inputs[3].checked)
         document.body.removeChild(e.target.parentNode)
         return 0;
+    }
+}
+
+function changePriorities(card, checked)
+{
+    if(no_prioties && checked)
+    {
+        no_prioties = false
+        let copy_container = other_container;
+        global_container.removeChild(other_container)
+
+        let title1 = document.createElement("h1")
+        title1.textContent = "Priority"
+        let title2 = document.createElement("h1")
+        title2.textContent = "Normal"
+        global_container.appendChild(title1)
+        global_container.appendChild(priority_container)
+        global_container.appendChild(title2)
+        global_container.appendChild(copy_container)
+    }
+    if(checked)
+    {
+        if(other_container.contains(card))
+        {
+            other_container.removeChild(card)
+        }
+        if(!priority_container.contains(card))
+        {
+            priority_container.appendChild(card)
+        }
+    }
+    if(!checked)
+    {
+        if(priority_container.contains(card))
+        {
+            priority_container.removeChild(card)
+        }
+        if(!other_container.contains(card))
+        {
+            other_container.appendChild(card)
+        }
+    }
+    
+    //console.log(checked)
+    //console.log(priority_container)
+    //console.log(other_container)
+}
+
+function updPriorities()
+{
+    if(!priority_container.childNodes.length && global_container.contains(priority_container))
+    {
+        console.log("KEK")
+        let prior_title = global_container.querySelector("h1")
+        global_container.removeChild(prior_title)
+        no_prioties = true
+        let other_title = global_container.querySelector("h1")
+        global_container.removeChild(other_title)
+        global_container.removeChild(priority_container)
+    }
+    
+}
+
+function saveImg(card, img_url)
+{
+    let img = card.querySelector("img")
+    console.log(img)
+    if(img)
+    {
+        img.src = img_url
+    }
+    else if(img_url){
+        
+        let title_ = card.querySelector(".title")
+        let text_  = card.querySelector(".text")
+        let btn    = card.querySelector(".action")
+
+        card.removeChild(title_)
+        card.removeChild(text_)
+        card.removeChild(btn)
+
+        let img_container = document.createElement("div")
+        img_container.classList.add("img_container")
+        let img = document.createElement("img")
+        img.src = img_url
+        img_container.appendChild(img)
+
+        card.appendChild(img_container)
+        card.appendChild(title_)
+        card.appendChild(text_)
+        card.appendChild(btn)
     }
 }
 
@@ -53,8 +146,9 @@ function deleteNote(card, edit_window)
     } 
     else if (priority_container.contains(card))
     {
-        priority_container.remove(card)
+        priority_container.removeChild(card)
     }
+    updPriorities()
     closeWindow(0, edit_window, card, false)
 }
 
@@ -96,8 +190,31 @@ function createEditWindow(card)
         text_inp.value = card.querySelector(".text").textContent
     text_inp.placeholder = "edit text"
 
+    let img_inp = document.createElement("input")
+    if(card.querySelector("img"))
+    {
+        console.log(card.querySelector("img"))
+        img_inp.value = card.querySelector("img").src;
+    }   
+    img_inp.placeholder = "enter img address"
+
+    let priority_inp = document.createElement("input")
+    priority_inp.type = "checkbox"
+    priority_inp.id = "make_priority"
+    if(priority_container.contains(card))
+    {
+        priority_inp.checked = true;
+    }
+
+    let checkbox_label = document.createElement("label")
+    checkbox_label.htmlFor = "make_priority"
+    checkbox_label.textContent = "make note priority"
+
     edit_window.appendChild(title)
     edit_window.appendChild(text_inp)
+    edit_window.appendChild(img_inp)
+    edit_window.appendChild(priority_inp)
+    edit_window.appendChild(checkbox_label)
     edit_window.appendChild(okey_button)
     edit_window.appendChild(cancel_button)
     edit_window.appendChild(delete_button)
@@ -143,6 +260,7 @@ add_btn.addEventListener("click", function (){
     {
         let new_card = createEmptyCard()
         openEditWindow(createEditWindow(new_card))
+        
         other_container.appendChild(new_card)
     }
 })
